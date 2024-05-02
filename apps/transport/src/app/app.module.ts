822324d3533b63de72bb2b42e1234b38665b26c5
amir <amir.ntit@gmail.com>
2024-05-02 18:16:31 +0330

    add& remove  some component to transport project

--- apps/transport/src/app/app.module.ts
+++ apps/transport/src/app/app.module.ts
@@ -3,15 +3,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 import { appRoutes } from './app.routes';
-import { NxWelcomeComponent } from './nx-welcome.component';
 import { Modules, Models } from '@total/core';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { SettingsComponent } from './components/settings/settings.component';
 @NgModule({
-  declarations: [AppComponent, NxWelcomeComponent],
+  declarations: [
+    AppComponent,
+    DashboardComponent,
+    SettingsComponent,
+  ],
   imports: [
     BrowserModule,
     RouterModule.forRoot(appRoutes),
-    Modules.ServiceRegistryModule.forRoot({ sender: Models.Enum.Projects.Transport })],
+    Modules.ServiceRegistryModule.forRoot({
+      sender: Models.Enum.Projects.Transport,
+    }),
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- apps/transport/src/app/app.routes.ts
+++ apps/transport/src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Route } from '@angular/router';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { SettingsComponent } from './components/settings/settings.component';
 
-export const appRoutes: Route[] = [];
+export const appRoutes: Route[] = [
+  { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
+  { path: 'dashboard', component: DashboardComponent },
+  { path: 'settings', component: SettingsComponent }
+];
