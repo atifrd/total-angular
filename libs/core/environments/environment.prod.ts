c84c969caf22b578677d56a27e4a03c91ca7e790
amir <amir.ntit@gmail.com>
2024-05-06 12:52:44 +0330

    add environment

---
+++ libs/core/environments/environment.prod.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  appVersion: 1,
+  production: true,
+  apiUrl: 'https://localhost:10001/'
+};
---
+++ libs/core/environments/environment.ts
@@ -0,0 +1,17 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  appVersion: 1,
+  production: false,
+  apiUrl: 'https://auth.total-soft.ir'
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
