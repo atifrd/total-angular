5aeb2d279822aebebe8908ccb828f2ed77629e08
amir <amir.ntit@gmail.com>
2024-05-06 12:55:21 +0330

    fix read translation from json files

--- libs/core/src/i18n/custom-translate-loader.ts
+++ libs/core/src/i18n/custom-translate-loader.ts
@@ -1,22 +1,22 @@
-// import * as en from '../../assets/jsons/en.json';
-// import * as fa from '../../assets/jsons/fa.json';
-// import * as cn from '../../assets/jsons/cn.json';
+import * as en from '../../assets/jsons/en.json';
+import * as fa from '../../assets/jsons/fa.json';
+import * as cn from '../../assets/jsons/cn.json';
 
 import { TranslateLoader } from '@ngx-translate/core';
 import { of } from 'rxjs';
 
 export class CustomTranslateLoader implements TranslateLoader {
   public getTranslation(lang: string) {
-    // if (lang === 'fa') {
-    //   return of(fa);
-    // }
-    // if (lang === 'en') {
-    //   return of(en);
-    // }
-    // if (lang === 'cn') {
-    //   return of(cn);
-    // }
-    // return of(en);
-    return of({})
+    if (lang === 'fa') {
+      return of(fa);
+    }
+    if (lang === 'en') {
+      return of(en);
+    }
+    if (lang === 'cn') {
+      return of(cn);
+    }
+    return of(en);
+    //return of({})
   }
 }
