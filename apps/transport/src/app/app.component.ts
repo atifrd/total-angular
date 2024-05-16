ca2722cd1352b135216803e69b8fa28d7a2fa376
atefeh <a.shahidyfard@gmail.com>
2024-05-16 13:27:20 +0330

    test mat compin material-componentsents in app.component

--- apps/transport/src/app/app.component.ts
+++ apps/transport/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Models, Auth, Mat } from '@total/theme';
 
 @Component({
   selector: 'app-transport-root',
@@ -7,4 +8,28 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'transport';
+  CupleDate = new Date();
+  options: string[] = ['One', 'Two', 'Three', 'ati', 'mali', 'ela', 'ala'];
+  selectedoption = 'mali';
+  price: string='1000000';
+  constructor(
+    private sdfsd: Auth.Services.AuthenticationService,
+    private _matSnackService: Mat.MatSnackService,
+    private _matDialogService: Mat.MatDialogService,
+  ) {}
+
+  xxxx() {
+    let d = this.options.slice();
+    console.log('..............', d, 'khodesh: ', this.options,'price is',this.price);
+
+    console.log(`cuple date is ${this.CupleDate}`);
+    // this._matSnackService.openSnackBar('hi','closeIt');
+    // this._matDialogService.openDeletePrompt().afterClosed().subscribe((result: any) => {
+
+    //   alert(result);
+    // })
+  }
+
+
+  
 }
