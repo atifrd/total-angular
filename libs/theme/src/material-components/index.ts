6728d2453167578e74903c53b3224c7f7a471dc2
atefeh <a.shahidyfard@gmail.com>
2024-06-01 18:30:30 +0330

    mat-tree-checkList with incorrect style loading

--- libs/theme/src/material-components/index.ts
+++ libs/theme/src/material-components/index.ts
@@ -2,6 +2,7 @@ import { MatCuppleDatepickerComponent } from './mat-datepicker/mat-cupple-datepi
 import { MatAutocompleteComponent } from './mat-autocomplete/mat-autocomplete.component';
 import { MatPriceInputComponent } from './mat-price-input/mat-price-input.component';
 import { MatDatatableComponent } from './mat-datatable/mat-datatable.component';
+import { MatTreeChecklistComponent } from './mat-tree-checklist/mat-tree-checklist.component';
 
 export * from './mat-snakbar/mat-snack.service';
 export * from './mat-dialog/mat-dialog.service';
@@ -11,4 +12,5 @@ export const materialComp = [
   MatAutocompleteComponent,
   MatPriceInputComponent,
   MatDatatableComponent,
+  MatTreeChecklistComponent
 ];
