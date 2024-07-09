d192d44598d65c2ef8103755ae82e7defb1168fb
aa <a.shahidyfard@gmail.com>
2024-07-09 18:30:44 +0330

    add test rout

--- apps/transport/src/app/app.routes.ts
+++ apps/transport/src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Route } from '@angular/router';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { SettingsComponent } from './components/settings/settings.component';
 import { CargoInfoComponent } from './components/transport/cargo/cargo-info/cargo-info.component';
+import { MatTreeChecklistComponent } from 'libs/theme/src/material-components/mat-tree-checklist/mat-tree-checklist.component';
 
 export const appRoutes: Route[] = [
   { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
@@ -9,5 +10,6 @@ export const appRoutes: Route[] = [
   { path: 'settings', component: SettingsComponent },
 
   ////
-  { path: 'cargo-info', component: CargoInfoComponent }
+  { path: 'cargo-info', component: CargoInfoComponent },
+  { path: 'tree', component: MatTreeChecklistComponent }
 ];
